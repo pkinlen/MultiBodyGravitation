88ebb65e6dacedb1e01bf3d8cd81696e991f2978
Philip Kinlen <philipkinlen@hotmail.com>
2020-09-16 22:53:12 +0100

    save angle theta

--- Body.java
+++ Body.java
@@ -3,6 +3,8 @@ public class Body{
      String m_name;
      double m_mass;          // kg
      double m_bodyRadius;    // meters, could be used for checking for collisions
+     double m_orbitRadius;   
+     double m_theta;         // initial angle
      
      double m_initPos0;      // initial position on axis 0 (meters)
      double m_initPos1;      // initial position on axis 1
@@ -19,22 +21,17 @@ public class Body{
                   double initPos0, double initPos1, double initPos2,
                   double initVel0, double initVel1, double initVel2){
 
-    	 initialize(name, mass, bodyRadius, initPos0, initPos1, initPos2, initVel0, initVel1, initVel2);    	 
+         m_name        = name;
+         m_mass        = mass;
+         m_bodyRadius  = bodyRadius;
+         m_oneOverMass = 1.0 / m_mass;
+
+    	 initialize(initPos0, initPos1, initPos2, initVel0, initVel1, initVel2);    	 
      }
      ///////////////////////////////////////////////////////////////////
-     public void initialize(String name,     double mass,     double bodyRadius, 
-                            double initPos0, double initPos1, double initPos2,
+     public void initialize(double initPos0, double initPos1, double initPos2,
                             double initVel0, double initVel1, double initVel2)  {
     	 
-    	  if( mass <= 0)
-    		  System.out.println(  "Mass should be greater than zero, for " 
-    	                         + name + " found it to be: " 
-    	                         + Double.toString(mass)                    );
-    	 
-          m_name        = name;
-          m_mass        = mass;
-          m_bodyRadius  = bodyRadius;
-          
           m_initPos0    = initPos0;
           m_initPos1    = initPos1;
           m_initPos2    = initPos2;
@@ -42,8 +39,6 @@ public class Body{
           m_initVel0    = initVel0;
           m_initVel1    = initVel1;
           m_initVel2    = initVel2;    
-          
-          m_oneOverMass = 1.0 / m_mass;
           
           /*
           System.out.println("Have body:  name: "         +                 name
@@ -58,28 +53,25 @@ public class Body{
           */
      }
      //////////////////////////////////////////////////////////////
-     public Body(String name, double mass, double bodyRadius, double orbitRadius, double theta, Config config){
-
+     public Body(String name, double mass, double bodyRadius, double orbitRadius, double theta){
     	 
+         m_name        = name;
+         m_mass        = mass;
+         m_bodyRadius  = bodyRadius;
+         m_orbitRadius = orbitRadius;
+         m_theta       = theta;
+         
+         m_oneOverMass = 1.0 / m_mass;
+    	 
+         setPosAndVel(orbitRadius, theta);
+     }
+     //////////////////////////////////////////////////////////////////
+     public void setPosAndVel(double orbitRadius, double theta){
          double initPos0    = orbitRadius * Math.cos(theta);
          double initPos1    = orbitRadius * Math.sin(theta);
          double initPos2    = 0.0;
          
          double initVel0, initVel1, initVel2;
-         
-         if( orbitRadius > 0) {
-        	 double sqrtGMoverR = Math.sqrt( Config.m_massOfSun * Config.m_gravitationalConst / orbitRadius);
-         
-        	 initVel0    = - Math.sin(theta) * sqrtGMoverR;
-        	 initVel1    =   Math.cos(theta) * sqrtGMoverR;
-        	 initVel2    =   0.0;    
-         } else {
-        	 initVel0   = 0.0;
-        	 initVel1   = 0.0;
-        	 initVel2   = 0.0;        	 
-         }
-         
-    	 initialize(name, mass, bodyRadius, initPos0, initPos1, initPos2, initVel0, initVel1, initVel2);
          
          /*
           *     F = ma
@@ -90,11 +82,30 @@ public class Body{
           *               = sqrt( G M / r) * ( -sin( theta + omega *t) , cos( theta + omega * t) )
           * 
           */
+         if( orbitRadius > 0) {
+        	 double sqrtGMoverR = Math.sqrt( Config.m_massOfSun * Config.m_gravitationalConst / orbitRadius);
+         
+        	 initVel0    = - Math.sin(theta) * sqrtGMoverR;
+        	 initVel1    =   Math.cos(theta) * sqrtGMoverR;
+        	 initVel2    =   0.0;    
+         } else {
+        	 initVel0    = 0.0;
+        	 initVel1    = 0.0;
+        	 initVel2    = 0.0;        	 
+         }
+         
+    	 initialize(initPos0, initPos1, initPos2, initVel0, initVel1, initVel2);
+          
      }     
+     //////////////////////////////////////////////////////////////
+     public void resetTheta(double theta){ 
+    	 setPosAndVel(m_orbitRadius, theta);
+     }
      //////////////////////////////////////////////////////////////
      public String   getName()        { return m_name;        }
      public double   getMass()        { return m_mass;        }
      public double   getBodyRadius()  { return m_bodyRadius;  }
+     public double   getTheta()       { return m_theta;       }
      
      public double   getInitPos0()    { return m_initPos0;    }
      public double   getInitPos1()    { return m_initPos1;    }
