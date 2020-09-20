3dc965395e4f1dd21ad67d53072eb2d0fa941017
Philip Kinlen <philipkinlen@hotmail.com>
2020-09-20 22:54:01 +0100

    polished the code a bit

--- FindPath.java
+++ FindPath.java
@@ -2,33 +2,41 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
+// This class will instantiate the BodiesMoving class which does the main 
+// work evaluating the motion of bodies under gravity.
+// FindPath:findPositionsForFlyBy() will attempt to find starting position of the 
+// planets to get close flybys for the meteorite. 
+// On the other hand, the method:
+// calculateMovementOfBodies() will just evaluate the paths taken by the 
+// bodies given a single initial position.
+
 public class FindPath {
 
-	private int m_meteoriteIdx;
+    private int m_meteoriteIdx;
     ////////////////////////////////////////////////////////////////////////////////
     public Config  getConfigData(boolean withOutput){
-    	
-    	double  flyByTargetDistance = 2e8;      // the closest distance the meteorite gets to the surface.
-    	
-    	double  timeStepInterval    = 1 * 30;   // seconds, ( model seems to work when when this kept well below shortest_orbit_period / 100
+        
+        double  flyByTargetDistance = 2e8;      // the closest distance the meteorite gets to the centre of the objects
+        
+        double  timeStepInterval    = 1 * 30;   // seconds, ( model seems to work when when this kept well below shortest_orbit_period / 100
                                                 // For the moon, the orbit_period is just less than 30 days.
                                                 // Could use 1 hour for m_timeStepInterval i.e. 3600 sec.
 
-    	double  totalTime           = 30 * 24 * 3600;   // total simulation time in seconds.
+        double  totalTime           = 30 * 24 * 3600;   // total simulation time in seconds.
         int     frames              = 200;               // approximate number of times the positions are output. I.e., number of frames in final animation.
-        int     numTimeSteps        = (int)(totalTime /timeStepInterval);        // For 8 bodes, with 100,000 time steps, calc time is about 1 second.
-        int     outputInterval      = (int)(totalTime/(timeStepInterval * frames));          // 1 means every time-step will be output, 2 means every second one, etc    	    	
-    	    	
-    	boolean displayFinalPos     = withOutput;
-    	boolean sendOutputToFile    = withOutput;
-    	  
-    	String  outputFilePath      = new String ("outputs/movingBodies_" + getTimeStampString() + ".csv");
-    	   	
+        int     numTimeSteps        = (int)(totalTime /timeStepInterval);             // For 8 bodes, with 100,000 time steps, calc time is about 1 second.
+        int     outputInterval      = (int)(totalTime/(timeStepInterval * frames));   // 1 means every time-step will be output, 2 means every second one, etc                
+                
+        boolean displayFinalPos     = withOutput;
+        boolean sendOutputToFile    = withOutput;
+          
+        String  outputFilePath      = new String ("outputs/movingBodies_" + getTimeStampString() + ".csv");
+               
         Config  config              = new Config( flyByTargetDistance, timeStepInterval, numTimeSteps,  outputInterval,
-        		                                  displayFinalPos,     sendOutputToFile, outputFilePath                  );
+                                                  displayFinalPos,     sendOutputToFile, outputFilePath                  );
         
         return config;
-        		
+                
    }
     /////////////////////////////////////////////////////////////
     public ArrayList<Body> getBodiesDataPolarCoords(){
@@ -37,7 +45,7 @@ public class FindPath {
         // 
         // For motion in a circular orbit round the sun: speed = sqrt(GM/r)
         // Where M is the mass of the sun, 
-   	    // and r is the distance between centre of body to centre of sun.
+        // and r is the distance between centre of body to centre of sun.
         //
         // For now we don't actually use the radii.  In this model we treat the bodies as point masses.
         
@@ -64,34 +72,34 @@ public class FindPath {
 
         m_meteoriteIdx = 10;
         //                  Name         Mass(kg),   Radius(m)     Initial Pos(m) 0,1,2    Initial Vel (m/s) 0,1,2
-        bodies.add(new Body("Meteorite", 3e9,        3e2,          8e9,  0,  0.0,          -2e6, 1e6, 0.0   )); 
+        bodies.add(new Body("Meteorite", 3e9,        3e2,          8e9,  0.0,  0.0,          -2e6, 1e6, 0.0   )); 
         
       
         return bodies;
    }          
     //////////////////////////////////////////////////////////
     public void addMoon(ArrayList<Body> bodies, double earthOrbitRadius, double earthTheta  ){
-    	
-    	double moonTheta        = earthTheta - Math.PI * 0.5;
-    	double moonOrbitRadius  = 4e8; // distance from earth to moon (m), should be centre to centre, rather than surface to surface
-    	
-        double moonPos0         = earthOrbitRadius * Math.cos(earthTheta) + moonOrbitRadius * Math.cos(moonTheta);	
-        double moonPos1         = earthOrbitRadius * Math.sin(earthTheta) + moonOrbitRadius * Math.sin(moonTheta); 	
+        
+        double moonTheta        = earthTheta - Math.PI * 0.5;
+        double moonOrbitRadius  = 4e8; // distance from earth to moon (m), should be centre to centre, rather than surface to surface
+        
+        double moonPos0         = earthOrbitRadius * Math.cos(earthTheta) + moonOrbitRadius * Math.cos(moonTheta);    
+        double moonPos1         = earthOrbitRadius * Math.sin(earthTheta) + moonOrbitRadius * Math.sin(moonTheta);     
         double moonPos2         = 0.0;
         
-   	    double sqrtGMoverREarth = Math.sqrt( Config.m_massOfSun   * Config.m_gravitationalConst / earthOrbitRadius);
-   	    double sqrtGMoverRMoon  = Math.sqrt( Config.m_massOfEarth * Config.m_gravitationalConst / moonOrbitRadius );
+        double sqrtGMoverREarth = Math.sqrt( Config.m_massOfSun   * Config.m_gravitationalConst / earthOrbitRadius);
+        double sqrtGMoverRMoon  = Math.sqrt( Config.m_massOfEarth * Config.m_gravitationalConst / moonOrbitRadius );
         
         double moonVel0         = - Math.sin(earthTheta) * sqrtGMoverREarth - Math.sin(moonTheta) * sqrtGMoverRMoon;
         double moonVel1         =   Math.cos(earthTheta) * sqrtGMoverREarth + Math.cos(moonTheta) * sqrtGMoverRMoon;
         double moonVel2         = 0.0;
         
-    	bodies.add(new Body("Moon",      
-    			            7.3e22,                        // mass of moon   (kg)
-    			            1.737e6,                       // radius of moon (m)
-    			            moonPos0, moonPos1, moonPos2,
-    			            moonVel0, moonVel1, moonVel2 ));
-    			            
+        bodies.add(new Body("Moon",      
+                            7.3e22,                        // mass of moon   (kg)
+                            1.737e6,                       // radius of moon (m)
+                            moonPos0, moonPos1, moonPos2,
+                            moonVel0, moonVel1, moonVel2 ));
+                            
 
     }
     //////////////////////////////////////////////////////////
@@ -101,7 +109,7 @@ public class FindPath {
         // 
         // For motion in a circular orbit round the sun: speed = sqrt(GM/r)
         // Where M is the mass of the sun, 
-   	 // and r is the distance between centre of body to centre of sun.
+        // and r is the distance between centre of body to centre of sun.
         //
         // For now we don't actually use the radii.  In this model we treat the bodies as point masses.
         
@@ -122,21 +130,20 @@ public class FindPath {
         bodies.add(new Body("Neptune",   1.02e24,    2.47e7,       4.5e12,  0.0,    0.0,        0.0,    5.43e3, 0.0    )); 
         bodies.add(new Body("Meteorite", 3e9,        3e2,          4.51e12, 1e10,   0.0,       -1e9,    0,      0.0    )); 
 
-      //bodies.add(new Body("Test",      10,         1,            1e11,    0.0,    0.0,        0.0,    36434.52363,  0.0    ));         
-      
         return bodies;
    }          
     ///////////////////////////////////////////////////////////
     public static void main(String[] args) {
-    	
+        
          System.out.println("Starting.");
          long startTime = System.nanoTime();
 
-         // NumericalSolver.testNumericalSolver();
+         //NumericalSolver.testNumericalSolver();
+         
          FindPath fp = new FindPath();
-    	 
-          //fp.findPositionsForFlyBy();
-          fp.calculateMovementOfBodies();
+         
+         //fp.findPositionsForFlyBy();   // this attempts to find starting positions to obtain flybys.
+         fp.calculateMovementOfBodies();
 
          double executionTime = 1e-9 * ( System.nanoTime() - startTime);
          System.out.println("Execution time is: " + Double.toString(executionTime) + " seconds.");          
@@ -146,87 +153,87 @@ public class FindPath {
     ///////////////////////////////////////////////
     public void findPositionsForFlyBy(){
 
-    	int firstPlanet = 1;
-    	int lastPlanet  = 8;
-    	
-    	Config config = getConfigData(false);
-    	
-    	double thetas[] = getInitialPlanetAngles(Math.max(firstPlanet, lastPlanet));
-    	reportFlyByDistances(thetas, config);
+        int firstPlanet = 1;
+        int lastPlanet  = 8;
+        
+        Config config = getConfigData(false);
+        
+        double thetas[] = getInitialPlanetAngles(Math.max(firstPlanet, lastPlanet));
+        reportFlyByDistances(thetas, config);
 
-    	for (int planet = firstPlanet; planet <= lastPlanet; planet++){
-    		
-    		NumericalSolver solver = getSolver(planet, thetas, config);
-    		thetas[planet] = solver.solve();
-    	}
-    	
-    	reportFlyByDistances(thetas, config);    	
+        for (int planet = firstPlanet; planet <= lastPlanet; planet++){
+            
+            NumericalSolver solver = getSolver(planet, thetas, config);
+            thetas[planet] = solver.solve();
+        }
+        
+        reportFlyByDistances(thetas, config);        
     }
     ////////////////////////////////////////////
-	private void reportFlyByDistances(double[] thetas, Config config){
-		
-		for (int planet = 0; planet <= 9; planet++){
-			
-			ArrayList<Body> bodies = getBodiesDataPolarCoords();
-	        BodiesMoving movingBodies = new BodiesMoving(bodies,config);
-	        double closestDist = movingBodies.calcClosestDist(planet, m_meteoriteIdx);
-			
-	        Body p = bodies.get(planet);
-	        
-	        if ( planet < thetas.length)
-	        	System.out.println("Found closest distance between the meteorite and " 
-	        						+ p.getName() + " is " + Double.toString(closestDist) 
-	        						+ " with theta: " + Double.toString(thetas[planet]));
-	        else
-	        	System.out.println("Found closest distance between the meteorite and " 
-						+ p.getName() + " is " + Double.toString(closestDist));
-		}
-	}
+    private void reportFlyByDistances(double[] thetas, Config config){
+        
+        for (int planet = 0; planet <= 9; planet++){
+            
+            ArrayList<Body> bodies = getBodiesDataPolarCoords();
+            BodiesMoving movingBodies = new BodiesMoving(bodies,config);
+            double closestDist = movingBodies.calcClosestDist(planet, m_meteoriteIdx);
+            
+            Body p = bodies.get(planet);
+            
+            if ( planet < thetas.length)
+                System.out.println("Found closest distance between the meteorite and " 
+                                    + p.getName() + " is " + Double.toString(closestDist) 
+                                    + " with theta: " + Double.toString(thetas[planet]));
+            else
+                System.out.println("Found closest distance between the meteorite and " 
+                        + p.getName() + " is " + Double.toString(closestDist));
+        }  // end of for loop
+    }
     ////////////////////////////////////////////
-	NumericalSolver  getSolver(final int planet, final double[] thetas, final Config config){
-		
-      	
-      	NumericalSolver.IfaceFnOneVariable fnOneVar = (new NumericalSolver.IfaceFnOneVariable()
-      	      {public double fn(double thetaOverRide) 
-      	         {return (findClosestPath(config, thetas, planet, thetaOverRide ));}  
-      	       } );
-      	
-      	double flyByTargetDist = config.getFlyByTargetDistance(); 
-      	double startTheta1     =  thetas[planet];
-      	double startTheta2     =  thetas[planet] + 0.01;
-      	double tol             =  1e-4;
-      	int    maxSteps        =  40;
-      	
-      	NumericalSolver solver = new NumericalSolver(flyByTargetDist, startTheta1, startTheta2, 
-      			                                     tol, maxSteps, fnOneVar);
-      	return solver;
-	}
+    NumericalSolver  getSolver(final int planet, final double[] thetas, final Config config){
+        
+          
+          NumericalSolver.IfaceFnOneVariable fnOneVar = (new NumericalSolver.IfaceFnOneVariable()
+                {public double fn(double thetaOverRide) 
+                   {return (findClosestPath(config, thetas, planet, thetaOverRide ));}  
+                 } );
+          
+          double flyByTargetDist = config.getFlyByTargetDistance(); 
+          double startTheta0     = thetas[planet];
+          double startTheta1     = thetas[planet] + 0.01;
+          double tol             = 1e-4;
+          int    maxSteps        = 40;
+          
+          NumericalSolver solver = new NumericalSolver(flyByTargetDist, startTheta0, startTheta1, 
+                                                       tol, maxSteps, fnOneVar);
+          return solver;
+    }
     ////////////////////////////////////////////
     private double findClosestPath(final Config config, final double[] thetas, final int planet, double thetaOverRide ){
 
-    	ArrayList<Body> bodies = getBodiesDataPolarCoords();
-    	for(int p = 0; p < thetas.length; p++) {
-    		if ( p == planet)
-    			bodies.get(p).resetTheta(thetaOverRide);
-    		else
-    			bodies.get(p).resetTheta(thetas[p]);    	
-    	}
-    	
+        ArrayList<Body> bodies = getBodiesDataPolarCoords();
+        for(int p = 0; p < thetas.length; p++) {
+            if ( p == planet)
+                bodies.get(p).resetTheta(thetaOverRide);
+            else
+                bodies.get(p).resetTheta(thetas[p]);        
+        }
+        
         BodiesMoving movingBodies = new BodiesMoving(bodies,config);
         double closestDist = movingBodies.calcClosestDist(planet, m_meteoriteIdx);
 
         return closestDist;
     }
-	////////////////////////////////////////////
-	// could get the output of first solver as the starting point for the next...
+    ////////////////////////////////////////////
+    // could get the output of first solver as the starting point for the next...
     private double[]  getInitialPlanetAngles(int maxPlanet){
-    	double[] thetas = new double[maxPlanet+1];
-    	
-    	ArrayList<Body> bodies = getBodiesDataPolarCoords();
-    	for(int p = 0; p < thetas.length; p++)
-    		thetas[p] = bodies.get(p).getTheta();
-    	
-    	return thetas;
+        double[] thetas = new double[maxPlanet+1];
+        
+        ArrayList<Body> bodies = getBodiesDataPolarCoords();
+        for(int p = 0; p < thetas.length; p++)
+            thetas[p] = bodies.get(p).getTheta();
+        
+        return thetas;
     }
 
     /////////////////////////////////////////////////
@@ -245,18 +252,21 @@ public class FindPath {
 }
 
 /* Possible extensions and improvements:
-bodies could spin
-could check for collisions, when have big steps, this is trickier...
-could check for overflow errors
-could fail more gracefully if the output file cannot be written to.
-look into possible division by zero errors, particularly if we don't have collision checks.
-could set better initial positions and velocities, could check if distances are to the surface or between centres.
-could remove the comma at the end of each line in the output file.
+  bodies could spin
+  could check for collisions, when have big steps
+  could check for overflow errors
+  could fail more gracefully if the output file cannot be written to.
+  look into possible division by zero errors, particularly if we don't have collision checks.
+  could set better initial positions and velocities, could check if distances are to the surface or between centres.
+  could remove the comma at the end of each line in the output file.
 
-One bug / feature of the model is that if the centre of two bodies are on a direct collision course.
-Then the objects can be predicted to accelerate massively, so much that they escape each others gravitational pull.
-Ultimately it is caused by using the reciprocal of distance between centres, which can be close to zero.
+  One bug / feature of the model is that if the centre of two bodies are on a direct collision course.
+  Then the objects can be predicted to accelerate massively, so much that they escape each others gravitational pull.
+  Ultimately it is caused by using the reciprocal of distance between centres, which can be close to zero.
 
-When large time steps are used relative to the orbit period, the model performance deteriorates.    
+  When large time steps are used relative to the orbit period, the model performance deteriorates.    
 
 */
+
+
+
