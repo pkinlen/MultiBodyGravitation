1432d69f4beec532eac96458793ee5cf38fcd3eb
Philip Kinlen <philipkinlen@hotmail.com>
2020-09-20 22:53:18 +0100

    mostly small changes, including to formatting

--- Config.java
+++ Config.java
@@ -1,7 +1,8 @@
-
+// The following class contains configuration data,
+// It is mostly used by the BodiesMoving class. 
 public class Config {
     private double  m_flyByTargetDistance; // The closest distance we target the meteorite to pass by each planet.
-	
+    
     private double  m_timeStepInterval;    // seconds, ( model seems to work when when this kept well below shortest_orbit_period / 100
                                            // For the moon, the orbit_period is just less than 30 days.
                                            // Could use 1 hour for m_timeStepInterval i.e. 3600 sec.
@@ -13,7 +14,7 @@ public class Config {
                                            //      m_numTimeSteps / m_outputInterval  
     private boolean m_displayFinalPos;
     
-	private boolean m_sendOutputToFile;
+    private boolean m_sendOutputToFile;
     private String  m_outputFilePath;
     
     private String  m_lineSep;
@@ -22,23 +23,23 @@ public class Config {
     public static final double m_massOfSun          = 1.989e30;      // kg
     public static final double m_massOfEarth        = 5.972e24;      // kg
 
-	/////////////////////////////////////////////
-	public Config(double  flyByTargetDistance,
-			      double  timeStepInterval, int     numTimeSteps,       int    outputInterval, 
-			      boolean displayFinalPos,  boolean sendOutputToFile, String outputFilePath  ){
-		
-		m_flyByTargetDistance = flyByTargetDistance; 
-		m_timeStepInterval    = timeStepInterval;
-		m_numTimeSteps        = numTimeSteps; 
-		m_outputInterval      = outputInterval;
-		m_displayFinalPos     = displayFinalPos;
-		m_sendOutputToFile    = sendOutputToFile;
-		m_outputFilePath      = outputFilePath;
-		
-		m_lineSep             = System.getProperty( "line.separator" );
-	}
-	/////////////////////////////////////////////
-	public double   getFlyByTargetDistance() { return m_flyByTargetDistance;  }
+    /////////////////////////////////////////////
+    public Config(double  flyByTargetDistance,
+                  double  timeStepInterval, int     numTimeSteps,       int    outputInterval, 
+                  boolean displayFinalPos,  boolean sendOutputToFile, String outputFilePath  ){
+        
+        m_flyByTargetDistance = flyByTargetDistance; 
+        m_timeStepInterval    = timeStepInterval;
+        m_numTimeSteps        = numTimeSteps; 
+        m_outputInterval      = outputInterval;
+        m_displayFinalPos     = displayFinalPos;
+        m_sendOutputToFile    = sendOutputToFile;
+        m_outputFilePath      = outputFilePath;
+        
+        m_lineSep             = System.getProperty( "line.separator" );
+    }
+    /////////////////////////////////////////////
+    public double   getFlyByTargetDistance() { return m_flyByTargetDistance;  }
     public double   getTimeStepInterval()    { return m_timeStepInterval;     }
     public int      getNumTimeSteps()        { return m_numTimeSteps;         }
     public int      getOutputInterval()      { return m_outputInterval;       }
@@ -46,5 +47,5 @@ public class Config {
     public boolean  sendOutputToFile()       { return m_sendOutputToFile;     }
     public String   getOutputFilePath()      { return m_outputFilePath;       }
     public String   getLineSep()             { return m_lineSep;              }
-	/////////////////////////////////////////////
+    /////////////////////////////////////////////
 }
